2c6d815950c0001a4892d47eae9053ea239524a9
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-03-26 16:21:23 +0100

    fix path to config file in mysql.sh

--- templates/mysql.sh
+++ templates/mysql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cat web/sites/default/settings.php | grep -E "^  'password"
+cat ~/drupal-civicrm/web/sites/default/settings.php | grep -E "^  'password"
 mysql -u {{pac}}_{{user}} {{pac}}_{{user}} -p
