667f48d18d2a8cec0b023c6a94d60d8978e75db0
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-03-31 10:08:50 +0200

    saas activate script: check for email address

--- templates/saas_activate.php
+++ templates/saas_activate.php
@@ -16,19 +16,14 @@ try {
   $DB_NAME = $databases['default']['default']['database'];
   $DB_USERNAME = $databases['default']['default']['username'];
   $DB_PASSWORD = $databases['default']['default']['password'];
+  $USER_EMAIL_ADDRESS = Get('UserEmailAddress', '');
+  if (empty($USER_EMAIL_ADDRESS)) {
+    echo '{"success": false, "msg": "missing email address"}';
+    exit(1);
+  }
   $pdo = new PDO('mysql:host=localhost;dbname='.$DB_NAME, $DB_USERNAME, $DB_PASSWORD);
   $statement = $pdo->prepare("update users_field_data set mail=:email, status=1, created=NOW(), changed=NOW() where name=:username and status=0");
-  $statement->execute(array(':email' => Get('UserEmailAddress', 'invalid@solidcharity.com'), ':username' => 'civi_admin'));
-
-  # initiate password reset, without sending the email
-  $token = Get('PasswordResetToken', 'invalid');
-  if ($token != 'invalid') {
-    $statement = $pdo->prepare("update public.password_reset set is_active = false where is_active = true");
-    $statement->execute();
-    $statement = $pdo->prepare("insert into public.password_reset(token, user_id, date_expiration, date_creation, ip, user_agent, is_active)".
-      " values(:token, 1, :date_expiration, :date_creation, '127.0.0.1', 'php', true)");
-    $statement->execute(array(':token' => $token, ':date_expiration' => time()+30*60, 'date_creation' => time()));
-  }
+  $statement->execute(array(':email' => $USER_EMAIL_ADDRESS, ':username' => 'civi_admin'));
 }
 catch (Exception $e) {
     // echo 'Exception caught: ',  $e->getMessage(), "\n";
